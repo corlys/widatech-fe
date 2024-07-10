491dfb00d7e2c9c3e0ca6f3985a576c4bd852560
corlys <duscae047@gmail.com>
2024-07-10 20:22:44 +0700

    feat: section 1, mvp form and search items function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,81 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
+
+const items = [
+  {
+    id: "1",
+    name: "Product 1",
+    pictureUrl: "https://example.com/product1.jpg",
+    stock: 100,
+    price: 29.99,
+  },
+  {
+    id: "2",
+    name: "Product 2",
+    pictureUrl: "https://example.com/product2.jpg",
+    stock: 50,
+    price: 59.99,
+  },
+  {
+    id: "3",
+    name: "Product 3",
+    pictureUrl: "https://example.com/product3.jpg",
+    stock: 200,
+    price: 19.99,
+  },
+];
 
 export default function HomePage() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredProduct = items.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery),
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#F7E7DC] to-[#FFF8F3] text-white">
+      <div className="container flex flex-row items-center justify-center gap-12 px-4 py-16 text-[#405D72]">
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="flex flex-col items-center justify-center gap-2"
+        >
+          <div className="flex flex-col items-start justify-between gap-2">
+            <label>CUSTOMER NAME </label>
+            <input type="text" name="customerName" />
+          </div>
+          <div className="flex flex-col items-start justify-between gap-2">
+            <label>SALES PERSON NAME </label>
+            <input type="text" name="salesPersonName" />
+          </div>
+          <div className="flex flex-col items-start justify-between gap-2">
+            <label>NOTES</label>
+            <textarea name="notes" />
+          </div>
+
+          <button
+            type="submit"
+            className="flex items-center justify-center rounded-xl bg-[#758694] px-2 py-2"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+            Submit
+          </button>
+        </form>
+        <div className="flex flex-col items-center justify-center">
+          <input
+            type="text"
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+          />
+          <div className="flex flex-col">
+            {filteredProduct.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </div>
         </div>
       </div>
     </main>
